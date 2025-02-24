7c65f53748341f530f928af776efcc685db8e26e
espmarchena <espemarchena26@gmail.com>
2025-02-24 13:32:35 +0100

    Add initial set of files

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInitfd0115627e097aebd3ce530b8a36e4bb
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitfd0115627e097aebd3ce530b8a36e4bb::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
